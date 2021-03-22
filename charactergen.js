fc1282d17a9c537a52a084703c0a16d84c9f4bdc
Max Chidzero <mwc349@nyu.edu>
2021-03-22 14:54:46 +0100

    first script

---
+++ charactergen.js
@@ -0,0 +1,38 @@
+const character = class {
+    constructor ( ) {
+        const names = ["Tropos", "Merk", "Elizbath", "Cornelius", "Zorg", "Phineas", "Neghet", "Cara", "Bon"];
+        const surnames = ["Tarak", "Tarth", "Barnsley", "Nobel", "Wildheart", "Fynntooth", "Fischer"];
+        const occupations = ["Mage", "Rogue", "Pirate", "Warrior", "Monk", "Explorer"];
+        const skills = ["Intellect", "Power", "Melee", "Healing", "Agility", "Support", "Strategy", "Speed", "Magic"];
+
+        this.name = names[Math.floor(Math.random() * names.length)];
+    
+        this.surname = surnames[Math.floor(Math.random() * surnames.length)];
+    
+        this.occupation = occupations[Math.floor(Math.random() * occupations.length)];
+    
+        this.skill = skills[Math.floor(Math.random() * skills.length)];
+    
+        this.level = Math.floor(Math.random() * 100);
+
+    }
+};
+
+var createMessage = () => {
+    /*Extra ToDo's: 
+        - add console.log output
+        - add styling to consol.log output w/ %c Deliiters
+        - add html/css/markdown front end
+    */
+    var myCharacter = new character();
+
+    let myName = `\n Name: \t ${myCharacter.name} ${myCharacter.surname}`
+    let myOccupation = `\n Occupation: \t ${myCharacter.occupation}`
+    let mySkill = `\n Skill: \t ${myCharacter.skill} `
+    let myLevel = `\n Level: \t ${myCharacter.level} `
+    let finalString = '%c' + myName + myOccupation + mySkill + myLevel;
+
+    return finalString;
+};
+
+console.log(createMessage(), 'color: hotpink;');
